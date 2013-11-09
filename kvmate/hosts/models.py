34867a40ebc74448fbc9f45fe312c44e4e22ba11
Daniel Nägele <danieln@selfnet.de>
2013-11-09 18:52:43 +0100

    populated host model

--- kvmate/hosts/models.py
+++ kvmate/hosts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Host(models.Model):
+    hostname = models.CharField(max_length=30, primary_key=True)
+    is_on = models.BooleanField()
+    has_salt = models.BooleanField()
+    upgrades = models.PositiveSmallIntegerField(default=1, blank=True, null=True)
+    def __unicode__(self):
+        return 'KVMate.Host %s [On: %d]' % (self.hostname, self.is_on)
