54cd0760d61745b8b8a3b2063e3ad52de8e2c620
Daniel Nägele <danieln@selfnet.de>
2013-11-11 13:09:32 +0100

    adjusted the host model again

--- kvmate/hosts/models.py
+++ kvmate/hosts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Host(models.Model):
     hostname = models.CharField(max_length=30, primary_key=True)
@@ -22,36 +24,12 @@ class Host(models.Model):
     def kill(self):
         pass
 
-    def set_mem(self):
-        pass
-
-    def set_vcpus(self):
-        pass
-
-    def set_disk(self):
-        pass
-
     def __unicode__(self):
         return 'KVMate.Host %s [On: %d]' % (self.hostname, self.is_on)
 
 # if the following hooks should be used, save using vhost.save(update_fields['vcpu'])
-# TODO: test if update_fields is a logical OR or an exact match
-@receiver(post_save, sender = Host, update_fields = ['vcpus'])
-def adjust_vcpus(sender, **kwargs):
-    backend.set_vcpu(kwargs['instance'])
-
-@receiver(post_save, sender = Host, update_fields = ['ram'])
-def adjust_ram(sender, **kwargs):
-    backend.set_ram(kwargs['instance'].ram)
-
-@receiver(post_save, sender = Host, update_fields = ['diskspace'])
-def adjust_diskspace(sender, **kwargs):
-    backend.set_diskspace(kwargs['instance'].diskspace)
-
-@receiver(post_save, sender = Host, update_fields = ['autostart'])
-def adjust_autostart(sender, **kwargs):
-    backend.set_autostart(kwargs['instance'].autostart)
-
-@receiver(post_save, sender = Host, update_fields = ['persistent'])
-def adjust_persistent(sender, **kwargs):
-    backend.set_persistent(kwargs['instance'].persistent)
+@receiver(post_save, sender = Host)
+def adjust_host(sender, **kwargs):
+    trigger_fields = ['vcpus', 'ram', 'diskspace', 'autostart', 'persistent']
+    updates = [trigger for trigger in kwargs['update_fields'] if trigger in trigger_fields]
+    backend.adjust(kwargs['instance'], updates)
