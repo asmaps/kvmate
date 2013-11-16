00ba9ad942f85c7e8731d97cffce322d64fb5a28
Daniel Nägele <danieln@selfnet.de>
2013-11-16 18:46:23 +0100

    externalized backends in an own app

---
+++ kvmate/backends/libvirt.py
@@ -0,0 +1,19 @@
+class LibvirtBackend():
+
+    def set_name(self, host, old_name):
+        pass
+
+    def set_state(self, host):
+        pass
+
+    def set_vcpus(self, host):
+        pass
+
+    def set_ram(self, host):
+        pass
+
+    def set_autostart(self, host):
+        pass
+
+    def set_persistent(self, host):
+        pass
