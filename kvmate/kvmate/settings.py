40ce8b341e6255cc6d5f90193672c7c93a53387c
Daniel Nägele <danieln@selfnet.de>
2013-11-16 18:47:27 +0100

    added apps as working in the settings

--- kvmate/kvmate/settings.py
+++ kvmate/kvmate/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'kvmate',
+    'host',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
