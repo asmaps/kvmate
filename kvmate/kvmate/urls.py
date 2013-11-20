cbbe4056bb2402177d0a1384fbcd3967b19c1a6d
Daniel Nägele <danieln@selfnet.de>
2013-11-20 20:52:40 +0100

    adjusted kvmate urls

--- kvmate/kvmate/urls.py
+++ kvmate/kvmate/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import RedirectView
+from django.contrib.auth.views import login, logout
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,7 +9,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'kvmate.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', RedirectView.as_view(url='/hosts', permanent=False), name='hosts'),
+    url(r'^login/$', login, {'template_name': 'kvmate/login.html'}, name='login'),
+    url(r'^logout/$', logout, {'template_name': 'kvmate/logout.html'}, name='logout'),
+
+    url(r'^$', RedirectView.as_view(url='/hosts', permanent=False)),
     url(r'^hosts/', include('host.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
