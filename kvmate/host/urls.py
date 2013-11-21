0b97a4c9eab54ac43e7b539280b6e208e74c720b
Daniel Nägele <danieln@selfnet.de>
2013-11-21 20:48:41 +0100

    added more functionality

--- kvmate/host/urls.py
+++ kvmate/host/urls.py
@@ -1,16 +1,26 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
-from .views import HostList, CreateHost
+from .views import HostListView, HostActionView, HostDetailView, HostCreateView
 
 urlpatterns = patterns("",
         url(
             regex=r'^$',
-            view=HostList.as_view(),
+            view=HostListView.as_view(),
             name="hosts"
             ),
         url(
             regex=r'^create/$',
-            view=CreateHost.as_view(),
+            view=HostCreateView.as_view(),
             name="create"
+            ),
+        url(
+            regex=r'^(?P<name>\w+)/$',
+            view=HostDetailView.as_view(),
+            name="info"
+            ),
+        url(
+            regex=r'^(?P<name>\w+)/(?P<action>\w+)/$',
+            view=HostActionView.as_view(),
+            name="action"
             ),
         )
--- kvmate/host/views.py
+++ kvmate/host/views.py
@@ -1,19 +1,53 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.views.generic import View, ListView, DetailView, CreateView
+from django.views.generic.edit import ModelFormMixin
 from .models import Host
 from .forms import HostForm
 from braces.views import LoginRequiredMixin
 
-class HostList(ListView):
+class HostListView(ListView):
     model = Host
 
-class CreateHost(LoginRequiredMixin, CreateView):
+    def get_queryset(self):
+        queryset = super(HostListView, self).get_queryset()
+        q = self.request.GET.get("q")
+        if q:
+            return queryset.filter(name__icontains=q)
+        return queryset
+
+class HostActionView(LoginRequiredMixin, View):
+    def get(self, request, name, action):
+        host = Host.objects.get(name=name)
+        if action == 'start' or action == 'poweron':
+            host.start()
+            return redirect('info', name=name)
+        elif action == 'reboot' or action == 'restart':
+            messages.add_message(request, messages.ERROR, 'restarted', 'success')
+            host.reboot()
+        elif action == 'halt' or action == 'shutdown' or action == 'poweroff':
+            host.halt()
+        elif action == 'kill' or action == 'forceoff':
+            host.kill()
+        return redirect('hosts')
+
+class HostDetailView(DetailView):
+    model = Host
+    slug_field = 'name'
+    slug_url_kwarg = 'name'
+
+    def get_context_data(self, **kwargs):
+        context = super(HostDetailView, self).get_context_data(**kwargs)
+        context['memory_in_mb'] = context['object'].memory/1024
+        return context
+
+class HostCreateView(LoginRequiredMixin, CreateView):
     model = Host
     form_class = HostForm
     template_name_suffix = '_create_form'
     success_url = '/'
-
-    initial = {'autostart': True, 'persistent': True }
+    initial = {'autostart': True, 'persistent': True, 'vcpus' : 1, 'memory' : 524288 }
 
     def form_valid(self, form):
-        return super(CreateHost, self).form_valid(form)
+        self.object = form.save()
+        return super(ModelFormMixin, self).form_valid(form)
