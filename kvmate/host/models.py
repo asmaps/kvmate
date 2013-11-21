f7ad8f1cf18daf2c1068a7eed7a5a7c1e574c74b
Daniel Nägele <danieln@selfnet.de>
2013-11-21 20:48:05 +0100

    made host models behave with the backend

--- kvmate/host/models.py
+++ kvmate/host/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from backends.libvirt import LibvirtBackend
+from backends.mylibvirt import LibvirtBackend
 
 class Host(models.Model):
-    name = models.CharField(unique = True, max_length=64)
+    name = models.SlugField(unique=True, max_length=64)
     is_on = models.BooleanField()
     # libvirt definitions (to be enforced on the hypervisor)
     vcpus = models.PositiveSmallIntegerField(verbose_name="Number of virtual CPUs")
     memory = models.PositiveIntegerField(verbose_name="Maximum amount of memory")
     autostart = models.BooleanField(verbose_name="Autostart this host")
     persistent = models.BooleanField(verbose_name="Make persistent")
+
+    lvb = LibvirtBackend()
+    field_map = {
+            'name' : lvb.set_name,
+            'is_on' : lvb.set_state,
+            'vcpus' : lvb.set_vcpus,
+            'memory' : lvb.set_memory,
+            'autostart' : lvb.set_autostart,
+            'persistent' : lvb.set_persistent
+            }
 
     def start(self):
         self.is_on = True
@@ -21,12 +31,12 @@ class Host(models.Model):
         self.save(update_fields=['is_on'])
 
     def reboot(self):
-        LibvirtBackend.reboot(self)
+        self.lvb.reboot(self)
 
     def kill(self):
         self.is_on = False
         self.save()
-        LibvirtBackend.destroy(self)
+        self.lvb.destroy(self)
 
     def __unicode__(self):
         return 'KVMate.Host %s [On: %d]' % (self.name, self.is_on)
@@ -37,23 +47,15 @@ class Host(models.Model):
         '''
         try:
             # intersect watched fields and update fields, save old values if needed
-            triggered = [field for field in kwargs['update_fields'] if field in field_map.keys()]
+            triggered = [field for field in kwargs['update_fields'] if field in self.field_map.keys()]
             if 'name' in triggered:
-                old_name = Host.objects.get(id=kwargs['instance'].id).name
+                old_name = Host.objects.get(id=self.id).name
             # enforce any changes made using the backends
-            field_map = {
-                    'name' : LibvirtBackend.set_name,
-                    'is_on' : LibvirtBackend.set_state,
-                    'vcpus' : LibvirtBackend.set_vcpus,
-                    'memory' : LibvirtBackend.set_memory,
-                    'autostart' : LibvirtBackend.set_autostart,
-                    'persistent' : LibvirtBackend.set_persistent
-                    }
             for field in triggered:
                 if field is 'name':
-                    field_map[field](kwargs['instance'], old_name)
+                    self.field_map[field](self, old_name)
                 else:
-                    field_map[field](kwargs['instance'])
+                    self.field_map[field](self)
         except KeyError:
             pass
         finally:
