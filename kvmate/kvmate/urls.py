f61553c73a979a21e2a54bc25a5f301288887a5b
Daniel Nägele <danieln@selfnet.de>
2013-11-26 01:02:15 +0100

    hooked up vnc and static files

--- kvmate/kvmate/urls.py
+++ kvmate/kvmate/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import RedirectView
 from django.contrib.auth.views import login, logout
@@ -9,11 +11,24 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'kvmate.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^login/$', login, {'template_name': 'kvmate/login.html'}, name='login'),
-    url(r'^logout/$', logout, {'template_name': 'kvmate/logout.html'}, name='logout'),
+    url(
+        regex=r'^login/$',
+        view=login,
+        kwargs={'template_name': 'kvmate/login.html'},
+        name='login'
+        ),
+    url(
+        regex=r'^logout/$',
+        view=logout,
+        kwargs={'template_name': 'kvmate/logout.html'},
+        name='logout'
+        ),
+    url(
+        regex=r'^$',
+        view=RedirectView.as_view(url='/hosts', permanent=False)
+        ),
 
-    url(r'^$', RedirectView.as_view(url='/hosts', permanent=False)),
     url(r'^hosts/', include('host.urls')),
-
+    url(r'^vnc/', include('vnc.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + staticfiles_urlpatterns()
