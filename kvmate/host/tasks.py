de929ce87af898196999e71d986de0232537781e
Daniel Nägele <danieln@selfnet.de>
2013-12-04 23:01:28 +0100

    possibly fixed some issues with hostupdates

--- kvmate/host/tasks.py
+++ kvmate/host/tasks.py
@@ -65,8 +65,8 @@ def update_hosts():
         domainlist.append(conn.lookupByName(name))
     # mirror or confirm them in our database
     for domain in domainlist:
-        try:
-            host = Host.objects.get(name=domain.name())
+        host = Host.objects.get(name=domain.name())
+        if host != None:
             logger.info('host %s found, checking for changes...' % host.name)
             new_data = {}
             new_data['is_on'] = True if domain.isActive() else False
@@ -96,7 +96,7 @@ def update_hosts():
                 host.persistent = new_data['persistent']
             host.save()
             dead_hosts.remove(host.name)
-        except Host.DoesNotExist:
+        else:
             logger.info('new host %s found, adding to database' % domain.name())
             host = Host.objects.create(
                 name=domain.name(),
