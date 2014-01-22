5704e453f17ad77cecbd91fd73fc5507dec71c92
Arne Schauf <arne@as-webservices.de>
2014-01-22 02:43:06 +0100

    import local_settings if the exist

--- kvmate/kvmate/settings/__init__.py
+++ kvmate/kvmate/settings/__init__.py
@@ -1,3 +1,8 @@
 from .dev import * # set your production settings here
 from .create import *
 from .vnc import *
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('No local settings found')
