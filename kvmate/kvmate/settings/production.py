23c6ec0e622dfd2f9857970d611b8aae799a1629
Daniel Nägele <danieln@selfnet.de>
2014-02-13 21:56:41 +0100

    change log location for deployed kvmate instances

--- kvmate/kvmate/settings/production.py
+++ kvmate/kvmate/settings/production.py
@@ -5,5 +5,6 @@ DEBUG = False
 
 # set logging verbosity
 LOGGING['handlers']['file']['level'] = 'INFO'
+LOGGING['handlers']['file']['filename'] = '../logs/kvmate.log'
 
 VIRTUALIZATION_BACKEND = 'backends.mylibvirt.LibvirtBackend'
