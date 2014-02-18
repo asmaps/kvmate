ab401c2424ed7f3b0147c5275be36586e284bbe8
Arne Schauf <arne@as-webservices.de>
2014-02-18 15:56:23 +0100

    fix log location

--- kvmate/kvmate/settings/production.py
+++ kvmate/kvmate/settings/production.py
@@ -1,10 +1,12 @@
 from .base import LOGGING
 
+import os
+
 # no debugging
 DEBUG = False
 
 # set logging verbosity
 LOGGING['handlers']['file']['level'] = 'INFO'
-LOGGING['handlers']['file']['filename'] = '../logs/kvmate.log'
+LOGGING['handlers']['file']['filename'] = os.path.expanduser('~/logs/kvmate.log')
 
 VIRTUALIZATION_BACKEND = 'backends.mylibvirt.LibvirtBackend'
